33d2944b5c37bfc7c58afd8738f64bed559b8cc1
Satscode <artsmartin44@gmail.com>
2026-01-02 15:45:43 +0300

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/cashu-bluetooth-wallet/',
+  base: '/satoshi-pay-wallet/',
   plugins: [react()],
   build: {
     rollupOptions: {
